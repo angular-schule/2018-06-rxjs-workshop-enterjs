d144cf8769841754a748bba7aab643d91462f5e3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-06-19 15:32:33 +0200

    error handling -  catchError

--- browser/src/app/exercises/4_error-handling/error-handling.component.ts
+++ browser/src/app/exercises/4_error-handling/error-handling.component.ts
@@ -19,7 +19,9 @@ export class ErrorHandlingComponent implements OnInit {
   start() {
     this.es.randomError().pipe(
 
-      retry(10)
+      retry(2),
+      // catchError(err => throwError('This was an error... (but we handled it) :-)'))
+      catchError(error => of('Was there an error?! No... :-)'))
 
     ).subscribe(
       value => this.logStream$.next(value),
